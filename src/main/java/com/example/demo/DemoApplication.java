5a00cfa8f9049294dbf17c6356b2f206481bf60c
Ramprasad V <vrprasad@gmail.com>
2022-11-24 06:45:21 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	String sayHello() {
+		return "Hello at "+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	}
+}
