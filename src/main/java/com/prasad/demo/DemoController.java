803fe20042c622198cac8cfa476dab5eb7c58da9
Ramprasad <vrprasad@gmail.com>
2023-05-28 11:26:27 +0530

    Added JAVA version check stage and also add timestamp to response

--- src/main/java/com/prasad/demo/DemoController.java
+++ src/main/java/com/prasad/demo/DemoController.java
@@ -1,15 +1,23 @@
 package com.prasad.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 @RestController
 public class DemoController {
 
     @RequestMapping("/hello")
-    String sayHello() {
-        return "Hello World";
+    ResponseEntity<String> sayHello() {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String responseText = "API healthcheck @ "+ timeStamp+" - All OK";
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body(responseText.toString());
     }
 
 }
